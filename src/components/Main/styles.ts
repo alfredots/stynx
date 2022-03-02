4c14bbb2e011522be73f0584b6a577856a74f43b
alfredo <alfredtito97@gmail.com>
2022-03-02 16:43:26 -0300

    feat: adicionado tema do styled components

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-
 export const Wrapper = styled.main`
   background-color: #06092b;
   color: #fff;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-
-import { GlobalStyles } from 'styles/global'
+import { GlobalStyles } from 'common/styles/global'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './../common/styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
@@ -19,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
